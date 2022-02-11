2b7b7428cafcc55f0db7701322f2146e4d1ed644
Mariana Sánchez <90398608+Mariana143@users.noreply.github.com>
2022-02-11 13:23:15 -0400

    Add files via upload
    EV MARIANA SÁNCHEZ 11/02/2022

---
+++ ev 1 progra III/ej 1/EV PROGRA III/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ej1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            int[] edad = new int[4];
+            string[] nombre = new string[4];
+            for (int i = 0; i < edad.Length; i++)
+            {
+                Console.WriteLine("Ingrese un nombre:"+i);
+                nombre[i] = Console.ReadLine();
+                Console.WriteLine("Ingrese una edad");
+                edad[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            string seudo;
+            //METODO BURBUJA
+            for (int i = 0; i < (edad.Length - 1); i++)
+            {
+                for (int j = 0; j < (edad.Length - 1); j++)
+                {
+                    if (edad[j] > edad[j + 1])
+                    {
+                        temp = edad[j];
+                        seudo = nombre[j];
+                        edad[j] = edad[j + 1];
+                        nombre[j] = nombre[j + 1];
+                        edad[j + 1] = temp;
+                        nombre[j + 1] = seudo;
+
+                    }
+                }
+
+            }
+            
+            Console.WriteLine("orden:");
+            for (int p = 0; p < edad.Length; p++)
+            {
+                Console.WriteLine(nombre[p] +':'+edad[p]);
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+    }
+
---
+++ ev 1 progra III/ej 2/ej 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ej_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Ingrese la longitud");
+            n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Ingrese un numero:", (i + 1));
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    int aux;
+                    if (vector[i] > vector[j])
+                    {
+                        aux = vector[i];
+                        vector[i] = vector[j];
+                        vector[j] = aux;
+                    }
+
+                }
+            }
+            Console.WriteLine("Ordenado");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
